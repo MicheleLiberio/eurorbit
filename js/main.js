77c081c0496fc908d8c3ae58f697ba8577904c32
MicheleLiberio <mikelelib@libero.it>
2025-09-07 20:04:00 +0200

    commit fix

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ const select=document.getElementById('select-cities');
 let gettingForecast=document.getElementById("getting-forecast");
 gettingForecast.style.display="none";
 
-let weatherError=document.getElementById("weather-error");
-weatherError.style.display="none";
 
 function getDate(date) {
 	let year=date.slice(0,4);
@@ -91,14 +89,35 @@ function buildWeatherInfo(data) {
 
 }
 
+function buildWeatherError(){
+	let mainDiv = document.createElement('div');
+	mainDiv.className = "weather-tab-error align-content-center my-2"
+	document.getElementById("weather-results").appendChild(mainDiv);
+
+	let middleDive = document.createElement('div');
+	middleDive.className = "weather-tab-error align-content-center my-2"
+	mainDiv.appendChild(middleDive);
+
+	let firstDiv = document.createElement('div');
+	firstDiv.className = "row"
+	firstDiv.outerHTML += '<svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">' +
+                            '<path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>' +
+                            '<path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>' +
+                          '</svg>'
+	middleDive.appendChild(firstDiv);
+
+	let secondDiv = document.createElement('div');
+	secondDiv.className = "row justify-content-center";
+	secondDiv.innerText = "Something went wrong...";
+	middleDive.appendChild(secondDiv);
+}
+
 async function getWeatherData(lat,lon) {
-	let weatherError=document.getElementById("weather-error");
-	weatherError.style.display="none";
-	weatherError.style.display="none";
 	const url=`https://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=civillight&output=json`;
 	try {
 		const response=await fetch(url);
 		if(!response.ok) {
+			buildWeatherError();
 			throw new Error(`Response status: ${response.status}`);
 		}
 
@@ -107,9 +126,8 @@ async function getWeatherData(lat,lon) {
 		buildWeatherInfo(result)
 	} catch(error) {
 		gettingForecast.style.display="none";
-		weatherError.style.display="flex";
 
-		//buildWeatherError();
+		buildWeatherError();
 		console.error(error.message);
 	}
 }
@@ -133,6 +151,7 @@ fetch('https://micheleliberio.github.io/eurorbit-coursera/json/weatherCities.jso
 // Evento per sapere quale opzione è stata scelta
 select.addEventListener('change',() => {
 	$('div[id^="id-weather-"]').remove();
+	$('div[id^="weather-error"]').remove();
 	document.getElementById("weather-start").style.display = "none";
 	gettingForecast.style.display="block";
 
